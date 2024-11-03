f71e1f354873077bb7e25e0163d84b9044fb630b
gilsonluzfilho <gilsonfilho1994@yahoo.com.br>
2024-11-02 22:45:16 -0300

    projeto atualizado

---
+++ script.js
@@ -0,0 +1,13 @@
+function enviarPedido(event) {
+  event.preventDefault(); // Impede o envio padrão do formulário
+
+  const pedido = document.getElementById('pedido').value; // Captura o valor do textarea
+  const numeroWhatsApp = '5584981089164'; // Substitua pelo número da sua loja no formato correto
+  const mensagem = encodeURIComponent(pedido); // Codifica a mensagem para URL
+
+  // Constrói a URL do WhatsApp
+  const urlWhatsApp = `https://wa.me/${84981089164}?text=${mensagem}`;
+
+  // Redireciona para o WhatsApp
+  window.open(urlWhatsApp, '_blank');
+}
