4235dd3507f32fffd00eb4b68b30d8243b8bbca5
minhapaginaweb <gilson.boyluz@hotmail.com>
2024-11-03 01:53:33 -0300

    projeto atualizado

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function enviarPedido(event) {
   event.preventDefault(); // Impede o envio padrão do formulário
 
   const pedido = document.getElementById('pedido').value; // Captura o valor do textarea
-  const numeroWhatsApp = '5584981089164'; // Substitua pelo número da sua loja no formato correto
+  const numeroWhatsApp = '5584988057989'; // Substitua pelo número da sua loja no formato correto
   const mensagem = encodeURIComponent(pedido); // Codifica a mensagem para URL
 
   // Constrói a URL do WhatsApp
-  const urlWhatsApp = `https://wa.me/${5584981089164}?text=${mensagem}`;
+  const urlWhatsApp = `https://wa.me/${5584988057989}?text=${mensagem}`;
 
   // Redireciona para o WhatsApp
   window.open(urlWhatsApp, '_blank');
